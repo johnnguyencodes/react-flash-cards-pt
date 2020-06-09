672655e885e8cf2d71d2419cd7715b85c1b42422
John Nguyen <jnguyen92780@gmail.com>
2020-06-08 17:38:52 -0700

    In App component, passed addCard() to createCard via props

--- src/components/create-card.jsx
+++ src/components/create-card.jsx
@@ -3,6 +3,14 @@ import React from "react";
 function CreateCard(props) {
   return (
     <h1>Create New Card</h1>
+    <form>
+      <label htmlFor="card-front">Question:</label>
+      <textarea name="card-front" id="card-front" cols="30" rows="10"></textarea>
+      <label htmlFor="card-back">Answer:</label>
+      <textarea name="card-back" id="card-back" cols="30" rows="10"></textarea>
+      <button type="submit" onClick={props.addCard} className="btn btn-success mr-3">Add</button>
+      <button type="reset" onClick={} className="btn btn-outline-dark">Cancel</button>
+    </form>
   );
 }
 
