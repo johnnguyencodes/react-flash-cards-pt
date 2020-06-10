a0969367f7d9e18803d1b02bbc0b775186f85fd3
John Nguyen <jnguyen92780@gmail.com>
2020-06-09 18:18:44 -0700

    in view-cards.jsx, the cards array from App's state was passed via props.  Then, in the Card component, each card was rendered with bootstrap.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -9,7 +9,28 @@ class App extends React.Component {
     super(props);
     this.state = {
       view: "view-cards",
-      cards: []
+      cards: [
+        {
+          question: "What is React?",
+          answer: "A JavaScript Framework."
+        },
+        {
+          question: "What is Node.js?",
+          answer: "JavaScript outside of a browser."
+        },
+        {
+          question: "What does HTML stand for?",
+          answer: "Hyper Text Markup Language."
+        },
+        {
+          question: "In React JSX, what do curly brackets represent?",
+          answer: "A JavaScript expression."
+        },
+        {
+          question: "What does CSS stand for?",
+          answer: "Cascading Style Sheet."
+        }
+      ]
     }
     this.setView = this.setView.bind(this);
     this.saveCards = this.saveCards.bind(this);
@@ -24,6 +45,7 @@ class App extends React.Component {
 
   saveCards() {
     localStorage.setItem("flash-cards", JSON.stringify(this.state.cards));
+    console.log(this.state.cards);
   }
 
   addCard(card) {
@@ -39,19 +61,20 @@ class App extends React.Component {
       case 'review-cards':
         return <ReviewCards />;
       case 'view-cards':
-        return <ViewCards />;
+        return <ViewCards cards={this.state.cards}/>;
       default:
         return null;
     }
   }
 
   render() {
-    console.log("Cards from App:", this.state.cards);
     const { viewState } = this.state.view;
     return (
-      <div className="text-center">
+      <div>
+        <div className="text-center col-10 offset-1">
         <Nav viewState={viewState} setView={this.setView} whatIsState={this.state.view}/>
         { this.getView() }
+      </div>
       </div>
     );
   }
